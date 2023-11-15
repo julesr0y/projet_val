857d1211321cf455600d1512098ae5be665ab9ee
Lucas03012004 <112948716+Lucas03012004@users.noreply.github.com>
2023-11-15 17:53:04 +0100

    class route
    début class route

--- out/build/x64-debug/cmake_install.cmake
+++ out/build/x64-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Admin/Repos/projet_val
+# Install script for directory: C:/Users/Admin/Documents/GitHub/projet_val
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/Admin/Repos/projet_val/out/install/x64-debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/Admin/Documents/GitHub/projet_val/out/install/x64-debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/Admin/Repos/projet_val/out/build/x64-debug/ProjetVal/cmake_install.cmake")
+  include("C:/Users/Admin/Documents/GitHub/projet_val/out/build/x64-debug/ProjetVal/cmake_install.cmake")
 
 endif()
 
@@ -46,5 +46,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Admin/Repos/projet_val/out/build/x64-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Admin/Documents/GitHub/projet_val/out/build/x64-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
